9550b45c7a58c5579d1486bbe5fc57bd1513b6f9
soulcure <colin.soulcurer@gmail.com>
2020-02-14 18:17:05 +0800

    dev update

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -3,16 +3,53 @@ package com.example.camera;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
 
 import com.example.image.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    CameraExample cameraExample;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+
+        findViewById(R.id.btn_open).setOnClickListener(this);
+        findViewById(R.id.btn_close).setOnClickListener(this);
     }
 
 
+    private void init() {
+        cameraExample = new CameraExample();
+        cameraExample.onCreate(this);
+
+    }
+
+
+    private void open() {
+        cameraExample.openCamera(new Handler());
+    }
+
+
+    private void close() {
+        cameraExample.closeCamera();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.btn_open) {
+            open();
+        } else if (id == R.id.btn_close) {
+            close();
+        } else {
+
+        }
+
+    }
 }
