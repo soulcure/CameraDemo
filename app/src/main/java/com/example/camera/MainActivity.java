931d599a3e0dd010461f9e9122e842498d378149
soulcure <colin.soulcurer@gmail.com>
2020-02-16 20:46:05 +0800

    dev update

--- app/src/main/java/com/example/camera/MainActivity.java
+++ app/src/main/java/com/example/camera/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.camera;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.os.Bundle;
-import android.os.Handler;
 import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 
 import com.example.image.R;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private static final String TAG = "MainActivity";
+
+    private static final String[] CAMERA_PERMISSION =
+            {Manifest.permission.CAMERA};
+
+    private static final int RC_CAMERA_PERM = 123;
+
+
+    private Context mContext;
     private Camera mCamera;
     private CameraPreview mPreview;
 
@@ -25,6 +39,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = this;
         preview = (FrameLayout) findViewById(R.id.camera_preview);
 
         init();
@@ -39,7 +55,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         cameraExample.onCreate(this);
 
     }
-
 
     private void open() {
         //cameraExample.openCamera(new Handler());
@@ -72,7 +87,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
 
         if (id == R.id.btn_open) {
-            open();
+            if (hasCameraPermission()) {
+                open();
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, RC_CAMERA_PERM);
+            }
+
+
         } else if (id == R.id.btn_close) {
             close();
         } else {
@@ -90,5 +111,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
+    private boolean hasCameraPermission() {
+        //使用兼容库就无需判断系统版本
+        int hasWriteStoragePermission = ContextCompat.checkSelfPermission(getApplication(), Manifest.permission.CAMERA);
+        if (hasWriteStoragePermission == PackageManager.PERMISSION_GRANTED) {
+            //拥有权限，执行操作
+            return true;
+        } else {
+            //没有权限，向用户请求权限
+            return false;
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        //通过requestCode来识别是否同一个请求
+        if (requestCode == RC_CAMERA_PERM) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //用户同意，执行操作
+                open();
+            } else {
+                //用户不同意，向用户展示该权限作用
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                    new AlertDialog.Builder(this)
+                            .setMessage(R.string.rationale_camera)
+                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    ActivityCompat.requestPermissions(MainActivity.this,
+                                            new String[]{Manifest.permission.CAMERA},
+                                            RC_CAMERA_PERM);
+                                }
+                            })
+                            .setNegativeButton("Cancel", null)
+                            .create()
+                            .show();
+                }
+            }
+        }
+    }
 
 }
